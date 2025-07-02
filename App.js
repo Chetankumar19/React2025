7ecebfa93a74f95a6ddd2dfa748571f91357dcbf
CHETAN KUMAR <chetan18026@gmail.com>
2025-07-02 19:53:09 +0530

    Finished third leacture

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",{
-    id:'heading'
-} , "Ram Ram balinder");
-console.log(heading)
+const Title = () => (
+    <div id="container">
+        <h1 className="heading">Badmasi nhi mitra</h1>
+    </div>
+     
+) ;
+
+const num =100;
+const num1 =10;
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">Hello mitra</h1>
+        <h2>{num + num1}</h2>
+        <h3>{Title()}</h3>
+    </div>
+     
+) ;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)   
+root.render(<HeadingComponent/> );
